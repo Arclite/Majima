d2ff18a11993a83f7ec17bf3ea7edfcc57ed2830
Geoff Pado <geoff@pado.name>
2018-01-23 21:13:34 -0800

    Fix Mac builds.

--- Majima/Majima.h
+++ Majima/Majima.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015年 松本宗太郎. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Majima.
 FOUNDATION_EXPORT double MajimaVersionNumber;
